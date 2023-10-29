c8d5648ec3a64a791ac9e5f6137e93c427479276
Yassine BIZNAOUNE <yassine.biznaoune@e-polytechnique.com>
2023-10-29 23:10:20 +0100

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font"
+import { useCallback } from "react"
+import * as SplashScreen from "expo-splash-screen"
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    regular: require("./assets/fonts/Poppins-Regular.ttf"),
+    extra_bold: require("./assets/fonts/Poppins-ExtraBold.ttf"),
+    light: require("./assets/fonts/Poppins-Light.ttf"),
+    medium: require("./assets/fonts/Poppins-Medium.ttf"),
+    semi_bold: require("./assets/fonts/Poppins-SemiBold.ttf")
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +39,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontFamily: "semi_bold",
+    fontSize: 24,
+    textAlign: "center"
+  }
 });
